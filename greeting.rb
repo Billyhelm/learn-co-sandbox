bb31d96001ca3cb0fe8eba1af2f9ff4e83c00372
Billy Helm <billyhelm88@gmail.com>
2020-08-03 15:48:06 +0000

    trying a new branch

---
+++ greeting.rb
@@ -0,0 +1,13 @@
+def greeting name
+  puts "hello #{name}"
+end
+
+def five
+  greeting 'billy'
+  greeting 'billy'
+  greeting 'billy'
+  greeting 'billy'
+  greeting 'billy'
+end
+
+five
---
+++ learning-comments.rb
@@ -0,0 +1,6 @@
+case greeting
+  when unfriendly_greeting
+    put "he's mean"
+  when friendly_greeting 
+    put "what a nice guy"
+end
