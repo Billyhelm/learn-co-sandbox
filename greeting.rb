59be1bfad0e4571d9251e29fe40d6c65b58ad57f
Billy Helm <billyhelm88@gmail.com>
2020-08-26 19:03:51 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,17 @@
+def greeting name
+  puts "hello #{name}"
+end
+
+def five
+  greeting 'billy'
+  greeting 'billy'
+  greeting 'billy'
+  greeting 'billy'
+  greeting 'billy'
+end
+
+five
+
+class MyClass
+  
+end 
